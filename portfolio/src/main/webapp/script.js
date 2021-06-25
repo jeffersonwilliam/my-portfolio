03d251eb5d42f5bbb2b0b21ad705560d8223829f
Jefferson Akesseh <wakesseh@gmail.com>
2021-06-25 03:36:23 +0000

    added translation library to portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,21 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-// function addRandomGreeting() {
-//   const greetings =
-//       ['Winter is coming', "DNA doesn't make a family. Love does.", 'When you play the Game of Thrones, you win or you die'];
-
-//   // Pick a random greeting.
-//   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-//   // Add it to the page.
-//   const greetingContainer = document.getElementById('greeting-container');
-//   greetingContainer.innerText = greeting;
-// }
-
 window.onload = () => {
   const navMenu = document.querySelector('.nav-menu');
   const navItems = document.querySelectorAll('.nav-item');
@@ -39,39 +24,31 @@ window.onload = () => {
   Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
 }
 
-/** Fetches the current date from the server and adds it to the page. */
-// async function showWelcomeMessage() {
-//   const responseFromServer = await fetch('/hello');
-//   const textFromResponse = await responseFromServer.json();
-
-//   const helloContainer = document.getElementById('hello-container');
-//   helloContainer.innerText = textFromResponse;
-
-// //   helloContainer.innerHTML = '';
-
-// //   reply.html = ' ';  
-
-//   const rand = Math.floor((Math.random() * 3) + 1);
-
-//   if (rand == 1){
-//     helloContainer.innerText = 'Name: ' + textFromResponse.Name
-//   }
-//   else if (rand == 2){
-//     helloContainer.innerText = 'Height: ' + reply.height
-//   }
-//   else{
-//     helloContainer.innerText = 'Color: ' + reply.color
-//   }
-// }
-
-
-
 async function showWelcomeMessage() {
-    const responseFromServer = await fetch("/hello");
-    const textFromResponse = await responseFromServer.json();
-    // console.log(textFromResponse);
-    const choice = textFromResponse[Math.floor(Math.random() * textFromResponse.length)];
+  const responseFromServer = await fetch("/hello");
+  const textFromResponse = await responseFromServer.json();
+  console.log(textFromResponse);
+  const choice = textFromResponse[Math.floor(Math.random() * textFromResponse.length)];
 
-    const responseContainer = document.getElementById("response-container");
-    responseContainer.innerText = choice;
+  const responseContainer = document.getElementById("response-container");
+  responseContainer.innerText = choice;
+}
+
+function requestTranslation() {
+  const text = document.getElementById('text').value;
+  const languageCode = document.getElementById('language').value;
+
+  const resultContainer = document.getElementById('result');
+  resultContainer.innerText = 'Loading...';
+
+  const params = new URLSearchParams();
+  params.append('text', text);
+  params.append('languageCode', languageCode);
+
+  fetch('/translate', {
+  method: 'POST',
+  body: params
+  }).then(response => response.text())
+  .then((translatedMessage) => {resultContainer.innerText = translatedMessage;
+  });
 }
